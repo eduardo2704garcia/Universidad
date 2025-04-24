be63e0c6da6540fa56aac1aa03083f0b3fb27105
Eduardo David García Romero <eduagarcia985@gmail.com>
2025-04-24 17:47:52 -0500

    Landing Page
    Similar Like a portfolio but this is about me

---
+++ LandingPage/script.js
@@ -0,0 +1,8 @@
+/*typing animations*/
+
+var typed = new Typed(".typing", {
+    strings: ["Web Designer", "Software Engineer", "Web Developer", "Machine Learning Engineer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+});
